ca930926a585d92fd9a5be642b9245c648722333
cgfor <70187893+cgfor@users.noreply.github.com>
2020-09-14 17:56:52 +0800

    Add files via upload

---
+++ data/cache/common_postimg.js
@@ -0,0 +1,1 @@
+var postimg_type = new Array();postimg_type["hrline"]=["line3.png","line5.png","line9.png","2.gif","4.gif","5.gif","line4.png","line7.png","line8.png","3.gif","0.gif","line1.png","line2.png","1.gif","line6.png"];postimg_type["postbg"]=["bg2.png","bg7.png","bg4.png","2.jpg","bg6.png","bg10.png","bg8.png","bg1.png","bg3.png","0.gif","3.jpg","bg5.png","1.jpg","bg9.png"];
