a2247b3b01312d16ddd5d9b47e193110e415038c
cgfor <70187893+cgfor@users.noreply.github.com>
2020-11-13 22:18:23 +0800

    Update recaptcham.js

--- data/cache/recaptcham.js
+++ data/cache/recaptcham.js
@@ -9,7 +9,7 @@
         var l9 = document.createElement('script');
         l9.src = 'https://www.recaptcha.net/recaptcha/api.js?onload=grec_ol';
         document.body.appendChild(l9);
-        var string = '<input name="seccodehash" type="hidden" value="' + idhash + '" /><span id="checkseccodeverify_' + idhash + '" style="display:none"><img src="static/image/common/check_right.gif" width="16" height="16" class="vm"></span><input name="seccodeverify" id="seccodeverify_' + idhash + '" type="hidden" value="' + idhash + '" /><div class="g-recaptcha" data-sitekey="6Lc9ncYZAAAAAGixx4tGz6CYcD4wwqcg1FH3VzuG"></div><span id="' + onloadid + '"><img src="static/image/common/loading.gif" class="vm"></span><a class="xi2" style="margin-left:6px" href="misc.php?mod=faq"><img src="static/image/common/info_small.gif" class="vm"></a>';
+        var string = '<input name="seccodehash" type="hidden" value="' + idhash + '" /><span id="checkseccodeverify_' + idhash + '" style="display:none"><img src="static/image/common/check_right.gif" width="16" height="16" class="vm"></span><input name="seccodeverify" id="seccodeverify_' + idhash + '" type="hidden" value="' + idhash + '" /><div class="g-recaptcha" data-sitekey="6Lc9ncYZAAAAAGixx4tGz6CYcD4wwqcg1FH3VzuG"></div><span id="' + onloadid + '"><img src="static/image/common/loading.gif" class="vm"></span>';
         $('input[name=seccodeverify]').parent().html(string);
         setTimeout(function() {
             dgebd(onloadid).innerHTML = data;
